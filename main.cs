aad02131503ce88987856c5f826222c504b6c55b
ZacharyLane <lane.zachary93@gmail.com>
2020-06-10 12:48:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+  Console.WriteLine ("Enter the name of item");
+    var name = Console.ReadLine();
+    Console.WriteLine("Enter the quantity");
+    var x = Console.ReadLine();
+    Console.WriteLine ("Enter the price");
+    var y = Console.ReadLine();
+    int x1 = Convert.ToInt32(x);
+    int y1 = Convert.ToInt32(y);
+    Console.WriteLine ("Total price of apple =");
+    Console.WriteLine (x1*y1);
+  }
+}
